c65c40d19a9578c1c93ec220b536bb0aca4d3ba5
soogil han <gkstnrlf7635@gmail.com>
2023-06-13 17:19:37 +0900

    마스터 커밋 1

---
+++ main.py
@@ -0,0 +1,28 @@
+# third에게 add.py add_func(정수, 정수), 리턴 정수
+# third에게 mul.py mul_func(정수, 정수), 리턴 정수
+# 나는 sub.py sub_func(정수, 정수), 리턴 정수
+# 나는 div.py div_func(정수, 정수), 리턴 정수
+from add import add_func
+from sub import sub_func
+from mul import mul_func
+from div import div_func
+## 함수
+
+
+
+## 변수
+num1, num2,res = 100, 200, 0
+
+
+## 메인
+res = add_func(num1, num2)
+print(res)
+
+res = sub_func(num1, num2)
+print(res)
+
+res = mul_func(num1, num2)
+print(res)
+
+res = div_func(num1, num2)
+print(res)
